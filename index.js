baefbd31aa0011ca6ff6a50984a8fe30f5f9f315
Érick <erickcontato@hotmail.com>
2019-02-12 13:44:08 -0200

    First commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const PORT = 3000;
+const app = express();
+
+app.get('/', (req, res) => res.json({status: "NTask API"}));
+app.listen(PORT, () => console.log(`NTAsk is alive - Porta ${PORT}`))
