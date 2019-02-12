2871c5f991ad9929ca10e42478ec22e7ad65b524
Érick <erickcontato@hotmail.com>
2019-02-12 13:55:26 -0200

    Implementando /task

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ const PORT = 3000;
 const app = express();
 
 app.get('/', (req, res) => res.json({status: "NTask API"}));
+
+app.get('/task', (req, res) => {
+    res.json({
+        task:[
+            {title: "Fazer Compras"},
+            {title: "Consertar o PC"}
+        ]
+    })
+})
+
 app.listen(PORT, () => console.log(`NTAsk is alive - Porta ${PORT}`))
