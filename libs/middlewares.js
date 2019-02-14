dee6b9eb2cfccffed62cbe0eed647de07a7b1789
Érick <erickcontato@hotmail.com>
2019-02-14 11:46:47 -0200

    On branch master  Changes to be committed: 	modified:   libs/middlewares.js 	modified:   package-lock.json 	modified:   package.json

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,9 +1,17 @@
 const bodyParser = require('body-parser')
+const cors = require('cors');
 
 module.exports = app => {
     app.set('port', 3000);
     
     app.use(bodyParser.json());
+    
+    app.use(cors({
+        origin: ["https://localhost:3001"],
+        methods: ["GET", "PUT", "POST", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"]
+    }))
+
     app.use((req, res, next) => {
         // middleware executado antes de qualquer outro middleware
         delete req.body.id;
